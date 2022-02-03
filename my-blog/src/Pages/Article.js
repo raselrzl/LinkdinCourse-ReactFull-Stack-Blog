c75a361e74703583a679a488cbbe9a86cf45a50d
Rasel Miah <rasel6041@gmail.com>
2022-02-04 00:17:26 +0100

    useParams

--- my-blog/src/Pages/Article.js
+++ my-blog/src/Pages/Article.js
@@ -1,7 +1,17 @@
 import React from 'react';
-
+import  { useParams } from 'react-router-dom';
+import ArticleContent from './ArticleContent'
 export default function Article() {
-  return <div>
-      <h1>this is article page</h1>
-  </div>;
+  const {name}=useParams();
+  const article=ArticleContent.find(article=>article.name===name);
+  if(!article) return <h1>Article doent exist</h1>
+  return (<div>
+      <h1>{article.title}</h1>
+      {ArticleContent.map((paragraph, key)=>{(
+        <p key={key}>{paragraph}</p>
+      )
+
+      })}
+  </div>
+  );
 }
---
+++ my-blog/src/Pages/ArticleContent.js
@@ -0,0 +1,15 @@
+const articles=[
+    {
+    name:'Learn-JS',
+    title:'The fastest way to learn js',
+    content:`hello jshello jshello jshello jshello jshello js`
+    },
+   {
+    name:'Learn-React',
+    title:'The fastest way to learn React',
+    content:`hello reacthello reacthello reacthello react`
+   }
+]
+
+export default articles
+
