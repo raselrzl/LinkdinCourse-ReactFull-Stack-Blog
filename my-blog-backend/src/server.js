d69ac66c82a8f554c73ce5d04104dece5e6db0fc
Rasel Miah <rasel6041@gmail.com>
2022-02-06 05:01:16 +0100

    adding comments

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -4,12 +4,15 @@ import bodyParser from 'body-parser'
 const articlesInfo={
     'react':{
         upvotes:0,
+        comments:[],
     },
     'node':{
         upvotes:0,
+        comments:[],
     },
     'resumes':{
         upvotes:0,
+        comments:[],
     },
 }
 
@@ -31,4 +34,15 @@ app.post('/api/articles/:name/upvote', (req, res)=>{
     res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!`)
 })
 
-app.listen(8000, ()=>console.log('listening'))
+app.post('/api/articles/:name/add-comment', (req, res)=>{
+    const {username ,text}=req.body;
+    const articleName=req.params.name;
+
+    articlesInfo[articleName].comments.push({username, text});
+
+    res.status(200).send(articlesInfo[articleName]);
+})
+
+
+
+app.listen(8000, ()=>console.log('listening on 8000'));
